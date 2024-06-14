3862f15b38a3927fda8ecd6c1c48dd93d405a5ff
talhagg <tgolcugezli@gmail.com>
2024-06-14 23:46:51 +0300

    Project templated completed.

---
+++ Case/RoversList/RoverListBuilder.swift
@@ -0,0 +1,8 @@
+//
+//  RoverListBuilder.swift
+//  Case
+//
+//  Created by Talha Gölcügezli on 14.06.2024.
+//
+
+import Foundation
---
+++ Case/RoversList/RoverListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RoverListViewModel.swift
+//  Case
+//
+//  Created by Talha Gölcügezli on 14.06.2024.
+//
+
+import Foundation
