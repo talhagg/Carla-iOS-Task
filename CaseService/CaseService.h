873302833339042ba38500799bb8967a9d046369
talhagg <tgolcugezli@gmail.com>
2024-06-14 22:09:11 +0300

    Initial Commit

---
+++ CaseService/CaseService.h
@@ -0,0 +1,18 @@
+//
+//  CaseService.h
+//  CaseService
+//
+//  Created by Talha Gölcügezli on 14.06.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CaseService.
+FOUNDATION_EXPORT double CaseServiceVersionNumber;
+
+//! Project version string for CaseService.
+FOUNDATION_EXPORT const unsigned char CaseServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CaseService/PublicHeader.h>
+
+
